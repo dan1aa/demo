9e4ab46241a2f41d16ddcaa3fe043a7c979862ba
dan1aa <burmeidan@gmail.com>
2021-09-28 18:12:39 +0300

    jsdoc edit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import {calcSalary} from "../modules/calcSalary.js"
 
-console.log(calcSalary([1,2,3,4,5,6,7,8,9]));
+console.log(calcSalary([1,2,3,4,5,6,7,8,9,10]));
 console.log(calcSalary([1]));
-console.log(calcSalary([9,8,4,3]));
+console.log(calcSalary([9,8,4,3,10]));
 console.log(calcSalary([1,1,1]));
 console.log(calcSalary('string'));
-console.log(calcSalary([2,3,3,3,4,5]));
+console.log(calcSalary([2,3,3,3,4,5, 10]));
 console.log(calcSalary([1,1,1,2,3,4,5,6,7,8,9]));
 console.log(calcSalary([]));
 console.log(calcSalary([9,9,9,9]));
